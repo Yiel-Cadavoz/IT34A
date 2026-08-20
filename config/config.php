8cc2c29028b845a2d5af751cbdc0a424f96bc2c5
Yiel-Cadavoz <cadavoz.arrieldenver@gmail.com>
2026-08-20 17:28:01 +0800

    added config/config.php and config/functions.php

---
+++ config/config.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+require_once('../includes/activity-logger.php');
+
+define('BASE_URL','http://localhost/it34a');
+
+define('DB_HOST','localhost');
+define('DB_NAME','it34a');
+define('DB_USER','root');
+define('DB_PASS','');
+
+$user_id = "root" ?? null;
+$user_email = "root" ?? null;
+
+try{
+    $pdo = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
+        DB_USER,
+        DB_PASS,
+        [PDO::ATTR_ERRMODE -> PDO::ERRMODE_EXCEPTION]
+    );
+    echo ("Connection Successful");
+    echo ($user_id . $user_email . 'connect_db','success');
+    logActivity($pdo,$user_id,$user_email,'connect_db','success');
+} catch(PDOException $e){
+    die("Connection Failed: " . $e->getMessage());
+}
+?>
